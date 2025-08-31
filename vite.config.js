61b388901af3be59c4029f688725839ad29523d2
Kalyan Kathuroji <kalyankathuroji@gmail.com>
2025-08-31 22:50:13 +0530

    Second Commit

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/Responsive-Portfolio-Web-Site",
+  base: "/Responsive-Portfolio-Web-Site/",
 });
