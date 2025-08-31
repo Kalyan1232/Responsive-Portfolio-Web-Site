cc4632104569c5d22f519261f666e4fb9a46f17b
Kalyan Kathuroji <kalyankathuroji@gmail.com>
2025-08-31 23:54:57 +0530

    Third Commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { HashRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <StrictMode>
     <App />
-  </HashRouter>,
+  </StrictMode>,
 )
