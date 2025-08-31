b32f5471f089c44b68885742ca1f067414f79304
Kalyan Kathuroji <kalyankathuroji@gmail.com>
2025-08-31 22:58:07 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import { Home } from "./Pages/Home.jsx";
 import { NotFound } from "./Pages/NotFound.jsx";
 import { Toaster } from "@/components/ui/toaster.jsx";
@@ -7,12 +7,12 @@ function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
