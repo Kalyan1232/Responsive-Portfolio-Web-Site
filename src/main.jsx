3e0973626be416851bd8c8468cd4072ecc6c657b
Kalyan Kathuroji <kalyankathuroji@gmail.com>
2025-09-01 23:17:55 +0530

    Third Commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
